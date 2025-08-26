dff84b53f1d18684faca0bd019dc5a78d3a733ba
kunash-media <kunashmediadesign15@gmail.com>
2025-08-26 16:36:23 +0530

    env set

--- src/main/java/com/ojal/OjalMicroFinanceApplication.java
+++ src/main/java/com/ojal/OjalMicroFinanceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OjalMicroFinanceApplication {
 
+//	Dotenv dotenv = Dotenv.configure().load();
+//	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 	public static void main(String[] args) {
 		SpringApplication.run(OjalMicroFinanceApplication.class, args);
 	}
