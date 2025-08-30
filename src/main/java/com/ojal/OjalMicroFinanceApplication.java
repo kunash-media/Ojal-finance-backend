047e7f2bda0641bf381062be49aad760a62ea0dc
kunash-media <kunashmediadesign15@gmail.com>
2025-08-30 14:59:39 +0530

    Update .env and application code, remove MessageService

--- src/main/java/com/ojal/OjalMicroFinanceApplication.java
+++ src/main/java/com/ojal/OjalMicroFinanceApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OjalMicroFinanceApplication {
-
-//	Dotenv dotenv = Dotenv.configure().load();
-//	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 	public static void main(String[] args) {
 		SpringApplication.run(OjalMicroFinanceApplication.class, args);
 	}
--- src/main/java/com/ojal/service/MessageService.java
+++
@@ -1,6 +0,0 @@
-package com.ojal.service;
-
-public interface MessageService {
-
-    void sendMessage(String to, String message);
-}
